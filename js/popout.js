0a7975c4468e025f82d9e51e5153d3250477671a
freelancerABD <72564444+freelancerABD@users.noreply.github.com>
2023-07-14 05:36:29 +0600

    Add files via upload

--- js/popout.js
+++ js/popout.js
@@ -1,11 +1,11 @@
-<!DOCTYPE html5>
-<html>
-<head>
-	<title>Popout Damage Counter</title>
-	<link rel="icon" type="image/x-icon" href="/favicon.ico">
-	<link href='//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,200italic,300italic,400italic' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" type="text/css" href="normalize.css">
-	<link rel="stylesheet" type="text/css" href="style.css">
+function openModalWindow() {
+	      var modalWindow = window.open("popout.html", "_blank", "width=500,height=600,left=400,top=60;");
+/*	      modalWindow.document.write(`
+	        <html>
+	          <head>
+	            <title>popout damge counter</title>
+	            <link rel="stylesheet" type="text/css" href="normalize.css">
+	
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 	<script type="text/javascript"  src="js/prefixfree.min.js"></script>
 	<script type="text/javascript"  src="js/less.min.js"></script>
@@ -286,102 +286,96 @@
 		}
 	</script>
 
-</head>
-	<body>
-	    <div class="popout container">
-		    <div class="popout-content">
-		    	<h4 class="tp-header">Damage Counter</h4>
-			 	<div class="col-lg-12">
-			  		<div class="row">
-				   		<div class="own ship col-6">
-						    <div class='center text-bt'>
-									Our ship:
-								<select name='ourShip' class="form-select">
-									<option value='sloop'>Sloop</option>
-									<option value='cutter'>Cutter</option>
-									<option value='dhow'>Dhow</option>
-									<option value='fanchuan'>Fanchuan</option>
-									<option value='longship'>Longship</option>
-									<option value='baghlah'>Baghlah</option>
-									<option value='mb'>Merchant Brig</option>
-									<option value='junk'>Junk</option>
-									<option value='wb'>War Brig</option>
-									<option value='mg'>Merchant Galleon</option>
-									<option value='wg'>War Galleon</option>
-									<option value='xebec'>Xebec</option>
-									<option value='wf'>War Frigate</option>
-									<option value='gf'>Grand Frigate</option>
-								</select>
-							</div>
-							<div class='shipPic'></div>
-							<div class='shipInfo'>
-								Shots to max/sink: <span id='ownHealth'>6</span> / <span id='ownSinkHealth'>10</span>
-							</div>
-							<div class='buttons left-btn pop-btn'>
-								<button name='weGotHit' class="btn">Got Shot</button>
-								<button name='weHitRocks' class="btn">Hit Rocks/Edge</button>
-							</div>
-						</div>
-			        	<div class="opponent ship col-6">
-			        		<div class='center text-bt'>
-								Their ship:
-								<select name='theirShip' class="form-select">
-									<option value='sloop'>Sloop</option>
-									<option value='cutter'>Cutter</option>
-									<option value='dhow'>Dhow</option>
-									<option value='fanchuan'>Fanchuan</option>
-									<option value='longship'>Longship</option>
-									<option value='baghlah'>Baghlah</option>
-									<option value='mb'>Merchant Brig</option>
-									<option value='junk'>Junk</option>
-									<option value='wb'>War Brig</option>
-									<option value='mg'>Merchant Galleon</option>
-									<option value='wg'>War Galleon</option>
-									<option value='xebec'>Xebec</option>
-									<option value='wf'>War Frigate</option>
-									<option value='gf'>Grand Frigate</option>
-								</select>
-							</div>
-							<div class='shipPic'></div>
-							<div class='shipInfo'>
-								Shots to max/sink: <span id='theirHealth'>6</span> / <span id='theirSinkHealth'>10</span>
-							</div>
-							<div class='buttons right-btn'>
-								<button name='theyGotHit' class="btn">Got Shot</button>
-								<button name='theyHitRocks' class="btn">Hit Rocks/Edge</button>
-							</div>
-			        	</div>
-			        	<div class="mid col-12">
-			        		<div style="font-size: 40px; font-weight: 700;" class="">VS</div>
-							<div class='scores'>
-								<div>Score: <span id='score'><span id='ourScore'>0</span> - <span id='theirScore'>0</span></span></div>
-								<div>SF Rows: <span id='rows'><span id='ourRows'>0</span> - <span id='theirRows'>0</span></span></div>
-								<button id="copyScore" class="btn btn-1 mt-1">Copy Score</button>
-								<button id="copyBoth" class="btn btn-2 my-2">Copy Score & Rows</button>
-							</div>
-							<div class="btn-b">
-								<button name='collision' class="btn">Collision</button>
-								<button name='reset' class="btn">Reset</button>
-							</div>
-							<button id="closePopup" class="btn btn-c" onclick="window.close()">
-								Close
-							</button>
-			        	</div>
-			        </div>
+	          </head>
+	          <body class="dark">
+	            <div class="popout container">
+		            <div class="popout-content">
+		            <h4 class="tp-header">Damage Counter</h4>
+			        	<div class="col-lg-12">
+			        		<div class="row">
+			        			<div class="own ship col-6">
+			        				<div class='center text-bt'>
+										Our ship:
+										<select name='ourShip' class="form-select">
+											<option value='sloop'>Sloop</option>
+											<option value='cutter'>Cutter</option>
+											<option value='dhow'>Dhow</option>
+											<option value='fanchuan'>Fanchuan</option>
+											<option value='longship'>Longship</option>
+											<option value='baghlah'>Baghlah</option>
+											<option value='mb'>Merchant Brig</option>
+											<option value='junk'>Junk</option>
+											<option value='wb'>War Brig</option>
+											<option value='mg'>Merchant Galleon</option>
+											<option value='wg'>War Galleon</option>
+											<option value='xebec'>Xebec</option>
+											<option value='wf'>War Frigate</option>
+											<option value='gf'>Grand Frigate</option>
+										</select>
+									</div>
+									<div class='shipPic'></div>
+									<div class='shipInfo'>
+										Shots to max/sink: <span id='ownHealth'>6</span> / <span id='ownSinkHealth'>10</span>
+									</div>
+									<div class='buttons left-btn pop-btn'>
+										<button name='weGotHit' class="btn">Got Shot</button>
+										<button name='weHitRocks' class="btn">Hit Rocks/Edge</button>
+									</div>
+			        			</div>
+			        			<div class="opponent col-6">
+			        				<div class='center text-bt'>
+										Their ship:
+										<select name='theirShip' class="form-select">
+											<option value='sloop'>Sloop</option>
+											<option value='cutter'>Cutter</option>
+											<option value='dhow'>Dhow</option>
+											<option value='fanchuan'>Fanchuan</option>
+											<option value='longship'>Longship</option>
+											<option value='baghlah'>Baghlah</option>
+											<option value='mb'>Merchant Brig</option>
+											<option value='junk'>Junk</option>
+											<option value='wb'>War Brig</option>
+											<option value='mg'>Merchant Galleon</option>
+											<option value='wg'>War Galleon</option>
+											<option value='xebec'>Xebec</option>
+											<option value='wf'>War Frigate</option>
+											<option value='gf'>Grand Frigate</option>
+										</select>
+									</div>
+									<div class='shipPic'></div>
+									<div class='shipInfo'>
+										Shots to max/sink: <span id='theirHealth'>6</span> / <span id='theirSinkHealth'>10</span>
+									</div>
+									<div class='buttons right-btn'>
+										<button name='theyGotHit' class="btn">Got Shot</button>
+										<button name='theyHitRocks' class="btn">Hit Rocks/Edge</button>
+									</div>
+			        			</div>
+			        			<div class="mid col-12">
+			        				<div style="font-size: 40px; font-weight: 700;" class="">VS</div>
+									<div class='scores'>
+										<div>Score: <span id='score'><span id='ourScore'>0</span> - <span id='theirScore'>0</span></span></div>
+										<div>SF Rows: <span id='rows'><span id='ourRows'>0</span> - <span id='theirRows'>0</span></span></div>
+										<button id="copyScore" class="btn btn-1 mt-1">Copy Score</button>
+										<button id="copyBoth" class="btn btn-2 my-2">Copy Score & Rows</button>
 
-			    </div>
+									</div>
+									<div class="btn-b">
+										<button name='collision' class="btn">Collision</button>
+										<button name='reset' class="btn">Reset</button>
+									</div>
+									<button id="closePopup" class="btn btn-c" onclick="window.close()">
+										Close
+									</button>
+			        			</div>
+			        		</div>
+
+			        	</div>
 			        	
-		    </div>
-		</div>
-		<!-- style switcher start - for demo purposse only -->
-		<div class="style-switcher outer-shadow" style="display:none;">
-			<div class="day-night s-icon outer-shadow hover-in-shadow">
-				<i class="fa-regular fa-moon"></i>
-			</div>
-		</div>
-		<!-- style switcher end -->
+		        	</div>
+		        </div>
 
-		<script src="js/d-dark.js"></script>
-		<script src="js/popout.js"></script>
-	</body>
-</html>
+	          </body>
+	        </html>
+	      `);*/
+	    }
