6e7cf4690e5034ba978e2292b5718c73e6e9822c
freelancerABD <72564444+freelancerABD@users.noreply.github.com>
2023-07-13 20:51:44 +0600

    Add files via upload

--- js/d-dark.js
+++ js/d-dark.js
@@ -37,3 +37,11 @@ function updateIcon(){
 		dayNight.querySelector("i").classList.add("fa-moon");
 	}
 }
+window.addEventListener("load", () =>{
+	if (document.body.classList.contains("dark")) {
+		dayNight.querySelector("i").classList.add("fa-sun");
+	}
+	else{
+		dayNight.querySelector("i").classList.add("fa-moon");
+	}
+})
